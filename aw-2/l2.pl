225ace59407d501bd6eeaacb5723cdf1591b851a
michalskim <legeof008@gmail.com>
2023-03-13 12:15:11 +0100

    chore: rework l3 names

--- aw-2/l2.pl
+++ aw-2/l2.pl
@@ -1,31 +1,31 @@
 ws(bielany,zoliborz,1).
-ws(bielany,bemowo,1).
-ws(bemowo,zoliborz,1).
-ws(bemowo,wola,1).
-ws(bemowo,wlochy,1).
-ws(wola,zoliborz,1).
-ws(wola,wlochy,1).
+ws(bielany,bemowo,2).
+ws(bemowo,zoliborz,4).
+ws(bemowo,wola,2).
+ws(bemowo,wlochy,4).
+ws(wola,zoliborz,2).
+ws(wola,wlochy,5).
 ws(wola,ochota,1).
-ws(wola,srodmiescie,1).
-ws(ochota,srodmiescie,1).
-ws(ochota,mokotow,1).
-ws(ochota,wlochy,1).
-ws(wlochy,mokotow,1).
-ws(wlochy,ursynow,1).
+ws(wola,srodmiescie,2).
+ws(ochota,srodmiescie,2).
+ws(ochota,mokotow,2).
+ws(ochota,wlochy,5).
+ws(wlochy,mokotow,4).
+ws(wlochy,ursynow,6).
 ws(ursynow,mokotow,1).
-ws(ursynow,wilanow,1).
-ws(mokotow,wilanow,1).
+ws(ursynow,wilanow,6).
+ws(mokotow,wilanow,3).
 ws(mokotow,srodmiescie,1).
 ws(srodmiescie,zoliborz,1).
-ws(bialoleka,bielany,1).
-ws(pragaN,zoliborz,1).
-ws(bialoleka,pragaN,1).
-ws(pragaN,srodmiescie,1).
-ws(pragaN,pragaS,1).
-ws(pragaS,srodmiescie,1).
-ws(pragaS,mokotow,1).
-ws(pragaS,wawer,1).
-ws(wawer,wilanow,1).
+ws(bialoleka,bielany,2).
+ws(pragaN,zoliborz,7).
+ws(bialoleka,pragaN,3).
+ws(pragaN,srodmiescie,7).
+ws(pragaN,pragaS,3).
+ws(pragaS,srodmiescie,7).
+ws(pragaS,mokotow,7).
+ws(pragaS,wawer,8).
+ws(wawer,wilanow,10).
 
 avoid([wlochy]).
 go(Here,There) :- route(Here, There,[Here],0). 
@@ -34,6 +34,7 @@ neighborroom(X,Y,C):- ws(X,Y,C).
 neighborroom(X,Y,C):- ws(Y,X,C).
 
 route(I,I,VisitedRooms,C) :-
+    member(srodmiescie,VisitedRooms),
     reverse(VisitedRooms,VisitedRoomsRealPath),
     write(VisitedRoomsRealPath),
     nl,
--- aw-3/l3-final.pl
+++ aw-3/l3-final.pl
@@ -1,27 +1,25 @@
 :-dynamic computer/5.
 
-displayMenu:-
-% Tu wszystko zgadza się do punktu 2 w pdfie
+display_menu:-
     write('1 - biezacy stan bazy danych'), nl,
     write('2 - dopisanie nowego komputera'), nl,
     write('3 - usuniecie komputera'), nl,
     write('4 - obliczenie sredniej ceny'), nl,
-% Te rzeczy kompletnie się nie zbiegają
     write('5 - wyszukanie procesorów po nazwie'), nl,
     write('6 - wyszukanie procesorów po cenie nizszej niz podana'), nl,
     write('Liczba spoza indeksu - koniec programu'), nl, 
     nl,
     read(I),
     I > 0,
-    menuOption(I),
-    displayMenu.
+    menu_option(I),
+    display_menu.
 
-displayMenu.
+display_menu.
 
-menuOption(1):-
-    displayDB.
+menu_option(1):-
+    display_db.
 
-menuOption(2) :- 
+menu_option(2) :- 
     write('Podaj nazwe procesora:'), 
     read(ProcessorName),
     write('Podaj typ procesora:'), 
@@ -35,17 +33,17 @@ menuOption(2) :-
     nl,
     assert(computer(ProcessorName, ProcessorType, ClockFrequency, HddSize, Price)).
 
-menuOption(3) :- 
+menu_option(3) :- 
     write('Podaj nazwe procesora do usuniecia:'), 
     read(ProcessorName),
     retract(computer(ProcessorName,_)),
     ! ;
     write('Brak takiego procesora ...').
 
-menuOption(4) :- 
-    averagePrice.
+menu_option(4) :- 
+    average_price.
 
-menuOption(5) :-
+menu_option(5) :-
     write('Podaj nazwe procesora do wyszukania'),
     read(ProcessorName),
     findall(computer(ProcessorName,ProcessorType,ClockFrequency,HddSize,Price), 
@@ -60,10 +58,10 @@ menuOption(5) :-
     write('Takich elementow jest '),
     write(ListLen),
     nl,nl,
-    displayComputerFromList(List,ListLen),
+    display_computer_from_list(List,ListLen),
     nl.
 
-menuOption(6) :-
+menu_option(6) :-
     write('Podaj prog cenowy do wyszukania ponizej'),
     read(PriceCap),
     findall(computer(ProcessorName,ProcessorType,ClockFrequency,HddSize,Price), 
@@ -78,10 +76,10 @@ menuOption(6) :-
     write('Takich elementow jest '),
     write(ListLen),
     nl,nl,
-    displayComputerFromList(List,ListLen),
+    display_computer_from_list(List,ListLen),
     nl.
 
-displayDB :- 
+display_db :- 
     write('Elementy bazy:'), 
     nl,
     computer(ProcessorName, ProcessorType, ClockFrequency, HddSize, Price),
@@ -97,10 +95,10 @@ displayDB :-
     nl, 
     fail.
 
-displayDB :- 
+display_db :- 
     nl.
 
-averagePrice :-
+average_price :-
     findall(Price, computer(_,_,_,_,Price), List),
     sum(List, Sum, NumberOfDbEntries),
     AveragePrice is Sum / NumberOfDbEntries,
@@ -115,10 +113,10 @@ sum([G|Og], Suma, N) :-
     Suma is G + S1,
     N is N1+1.
 
-displayComputerFromList([],0).
+display_computer_from_list([],0).
     
-displayComputerFromList([Head|Tail],ListEntryIndexNext):-
-    displayComputerFromList(Tail,ListEntryIndex),
+display_computer_from_list([Head|Tail],ListEntryIndexNext):-
+    display_computer_from_list(Tail,ListEntryIndex),
     write(Head),
     nl,
     ListEntryIndexNext is ListEntryIndex + 1.
